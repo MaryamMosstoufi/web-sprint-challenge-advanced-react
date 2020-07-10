8a62ae6b4598c27d9f6257aa13a15d4ef2c5043d
MaryamMosstoufi <maryam@zooxoos.com>
2020-07-10 12:19:04 -0400

    Create and Import useForm

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,11 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from "react";
+
+export const useForm = () => {
+  
+  return []
+};
+
+
+export default useForm;
